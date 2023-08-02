8c1d951c5f358aa0d3488f1de1c8b64d00b791c4
Fabio de Stefani <fabiostefani@gmail.com>
2023-08-01 21:37:54 -0300

    ms

---
+++ cmd/walletcore/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"database/sql"
+	"ex_ms_walletcore/internal/database"
+	"ex_ms_walletcore/pkg/events"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", "root", "root", "mysql", "3306", "wallet"))
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	eventDispatcher := events.NewEventDispatcher()
+	//eventDispatcher.Register("TransactionCreated", handler)
+
+	clientDb := database.NewClientDB(db)
+	accountDb := database.NewAccountDB(db)
+	transactionDb := database.NewTransactionDB(db)
+
+	createClientUseCase := createclient.NewCreateClientUseCase(clientDb)
+	createAccountUseCase := createaccount.NewCreateAccountUseCase(accountDb, eventDispatcher, event.NewAccountCreated())
+}
